cd4f01726228f9dbf2e797fe4f2bb0eb9ab1f866
Trebulkins <trebinivan2005@gmail.com>
2025-10-22 10:34:30 +0700

    ver 0.3

--- app/src/main/java/com/example/nm_lab4_geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/nm_lab4_geoquiz/MainActivity.kt
@@ -21,6 +21,8 @@ import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -34,12 +36,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val num = remember{ mutableIntStateOf(0) }
-            val result = remember{ mutableIntStateOf(0) }
+            val num = rememberSaveable{ mutableIntStateOf(0) }
+            val result = rememberSaveable{ mutableIntStateOf(0) }
             NM_lab4_GeoQuizTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Column(Modifier.padding(innerPadding)) {
-                        AnswerMe(modifier = Modifier.fillMaxWidth(), num = num, result = result)
+                        AnswerMe(modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally), num = num, result = result)
                     }
                 }
             }
@@ -65,59 +67,56 @@ fun answerfun(ans: Boolean, num: MutableIntState, result: MutableIntState) {
 
 @Composable
 fun AnswerMe(modifier: Modifier, num: MutableIntState, result: MutableIntState) {
-    val isnext = remember{ mutableStateOf(false) }
-    val isend = false
-    Column(modifier) {
-        Spacer(Modifier.height(10.dp))
+    val isnext = rememberSaveable{ mutableStateOf(false) }
+    Spacer(Modifier.height(10.dp))
+    AnimatedVisibility(visible = num.value >= 6) {
+        Text(
+            text = "Реузльтат: ${result.value} / 6",
+            modifier = modifier,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold,
+            fontSize = 28.sp
+        )
+    }
+    AnimatedVisibility(visible = num.value < 6) {
+        Text(
+            text = questions[num.value],
+            modifier = modifier,
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+    Spacer(Modifier.height(10.dp))
+    AnimatedVisibility(visible = !isnext.value && num.value < 6) {
+        Row(
+            modifier = modifier,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button({ answerfun(true, num, result); isnext.value = true }) {
+                Text(text = "Правда")
+            }
+            Button({ answerfun(false, num, result); isnext.value = true }) {
+                Text(text = "Ложь")
+            }
+        }
+    }
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        AnimatedVisibility(visible = isnext.value) {
+            Button({ num.value += 1; isnext.value = false }, Modifier.fillMaxWidth(0.5f)) {
+                Text(text = "Далее")
+            }
+        }
+    }
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceAround
+    ) {
         AnimatedVisibility(visible = num.value >= 6) {
-            Text(
-                text = "Реузльтат: ${result.value} / 6",
-                modifier = modifier,
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Bold,
-                fontSize = 28.sp
-            )
-        }
-        AnimatedVisibility(visible = num.value < 6) {
-            Text(
-                text = questions[num.value],
-                modifier = modifier,
-                textAlign = TextAlign.Center,
-                fontSize = 20.sp
-            )
-        }
-        Spacer(Modifier.height(10.dp))
-        AnimatedVisibility(visible = !isnext.value) {
-            Row(
-                modifier = modifier,
-                horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Button({ answerfun(true, num, result); isnext.value = true }) {
-                    Text(text = "Правда")
-                }
-                Button({ answerfun(false, num, result); isnext.value = true }) {
-                    Text(text = "Ложь")
-                }
-            }
-        }
-        Row(
-            modifier = modifier,
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            AnimatedVisibility(visible = isnext.value) {
-                Button({ num.value += 1; isnext.value = false }, Modifier.fillMaxWidth(0.5f)) {
-                    Text(text = "Далее")
-                }
-            }
-        }
-        Row(
-            modifier = modifier,
-            horizontalArrangement = Arrangement.SpaceAround
-        ) {
-            AnimatedVisibility(visible = num.value >= 6) {
-                Button({ num.value = 0; isnext.value = false }, Modifier.fillMaxWidth(0.5f)) {
-                    Text(text = "Заново")
-                }
+            Button({ num.value = 0; isnext.value = false; result.value = 0 }, Modifier.fillMaxWidth(0.5f)) {
+                Text(text = "Заново")
             }
         }
     }
@@ -126,7 +125,13 @@ fun AnswerMe(modifier: Modifier, num: MutableIntState, result: MutableIntState) 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+    val num = remember{ mutableIntStateOf(0) }
+    val result = remember{ mutableIntStateOf(0) }
     NM_lab4_GeoQuizTheme {
-        AnswerMe(modifier = Modifier.fillMaxSize(), num = remember{mutableIntStateOf(0)}, result = remember{mutableIntStateOf(0)})
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(Modifier.padding(innerPadding)) {
+                AnswerMe(modifier = Modifier.fillMaxWidth(), num = num, result = result)
+            }
+        }
     }
 }
